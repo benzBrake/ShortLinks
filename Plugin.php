06cfa67c4b9c7296e676b013ff245ba1305ac753
Char1sma <github-charisma@32mb.cn>
2016-04-14 22:00:35 +0800

    First Commit

---
+++ Plugin.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * 把外部链接转换为 your_blog_path/t/key/  <br>
+ * 或者 your_blog_path/go/key/ <br>
+ * 通过菜单“创建->短链接”设置 <br>
+ * 自定义短链功能来自<a href="http://defe.me/prg/429.html">golinks</a> | 感谢：<a href="http://forum.typecho.org/viewtopic.php?t=5576">小咪兔</a>
+ *
+ * @package ShortLinks
+ * @author Ryan
+ * @version 1.0.1
+ * @link http://blog.iplayloli.com/typecho-plugin-shortlinks.html
+ */
+ class ShortLinks_Plugin implements Typecho_Plugin_Interface
+ {
+	 /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return String
+     * @throws Typecho_Plugin_Exception
+     */
+	 public static function activate()
+    {
+		$db = Typecho_Db::get();
+        $shortlinks = $db->getPrefix() . 'shortlinks';
+        $adapter = $db->getAdapterName();
+        if("Pdo_SQLite" === $adapter || "SQLite" === $adapter){
+           $db->query(" CREATE TABLE IF NOT EXISTS ". $shortlinks ." (
+               id INTEGER PRIMARY KEY, 
+               key TEXT,
+               target TEXT,
+               count NUMERIC)");
+        }
+        if("Pdo_Mysql" === $adapter || "Mysql" === $adapter){
+            $db->query("CREATE TABLE IF NOT EXISTS ". $shortlinks ." (
+                  `id` int(8) NOT NULL AUTO_INCREMENT,
+                  `key` varchar(64) NOT NULL,
+                  `target` varchar(10000) NOT NULL,
+                  `count` int(8) DEFAULT '0',
+                  PRIMARY KEY (`id`)
+                ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1");
+        }
+
+        Helper::addAction('shortlinks', 'ShortLinks_Action');
+        Helper::addRoute('t', '/t/[key]/', 'ShortLinks_Action', 'baselink');
+		Helper::addRoute('go', '/go/[key]/', 'ShortLinks_Action', 'shortlink');
+        Helper::addPanel(2, 'ShortLinks/panel.php', '短链接', '短链接管理',   'administrator');
+		Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('ShortLinks_Plugin','replace');
+		Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('ShortLinks_Plugin','replace');
+		Typecho_Plugin::factory('Widget_Abstract_Comments')->contentEx = array('ShortLinks_Plugin','replace');
+        return('数据表 '.$shortlinks.' 创建成功, 插件已经成功激活!');
+    }
+	/**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return String
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+    {
+        Helper::removeRoute('go');
+		Helper::removeRoute('t');
+        Helper::removeAction('shortlinks');
+        Helper::removePanel(2, 'ShortLinks/panel.php');
+		return('短链接插件已被禁用，但是数据表并没有被删除');
+    }
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+		$convert =  new Typecho_Widget_Helper_Form_Element_Radio('convert' , array('1'=>_t('开启'),'0'=>_t('关闭')),'1',_t('外链转内链'),_t('会帮你把外链转换成内链'));
+		$form->addInput($convert );
+    }
+
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+	/**
+     * 外链转内链
+     *
+     * @access public
+     * @param $content
+	 * @param $class
+     * @return $content
+     */
+	public static function replace($text, $widget, $lastResult) {
+		if(Typecho_Widget::widget('Widget_Options')->Plugin('ShortLinks')->convert == 1)  {
+			$text = empty($lastResult) ? $text : $lastResult;
+			if (($widget instanceof Widget_Archive)||($widget instanceof Widget_Abstract_Comments)) {
+				$options = Typecho_Widget::widget('Widget_Options');
+				$sUrl = str_ireplace('/', '\/', rtrim($options->siteUrl, '/'));
+				$preg = '#(<a .*?href=")(?!' . $sUrl . ')([^"]+)"(.*?<\/a>)#ise';
+				$text = preg_replace($preg, "stripslashes('$1') . '$options->siteUrl' . 't/' . base64_encode('$2') . '\" target=\"_blank\"' . stripslashes('$3')", $text);
+			}
+			if ($widget instanceof Widget_Abstract_Comments) {
+				$widget->url = $options->siteUrl .  "t/" .base64_encode($widget->url );
+			}
+		}
+		return $text;
+	}
+ }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ShortLinks
+有时候我们为了减少权重的流失，或者是为了隐藏某些推荐链接（比如淘宝客、主机推荐），我们需要将外链转化为内链（淘宝客、主机推荐都是是隐藏AFF）。
+
 Typecho外链转内链插件，支持正文和评论者链接
+
+### 简介
+把外部链接转换为 your_blog_path/t/key/
+或者 your_blog_path/go/key/ <br>
+通过菜单“创建->短链接”设置 <br>
+自定义短链功能来自[golinks](http://defe.me/prg/429.html "golinks")
+
+### 感谢
+[小咪兔](http://forum.typecho.org/viewtopic.php?t=5576 "小咪兔")
+
+[DEFE](http://defe.me/prg/429.html "golinks")
+
+### 预览
+暂时没有
