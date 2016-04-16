e93f3a016fad638b1e03d8e0b6dc55a8c872f0e9
Char1sma <webmaster@iplayloli.com>
2016-04-16 17:39:56 +0800

    Format Codes fix 404 Problem

--- Plugin.php
+++ Plugin.php
@@ -13,89 +13,89 @@
  class ShortLinks_Plugin implements Typecho_Plugin_Interface
  {
 	 /**
-     * 激活插件方法,如果激活失败,直接抛出异常
-     *
-     * @access public
-     * @return String
-     * @throws Typecho_Plugin_Exception
-     */
+	 * 激活插件方法,如果激活失败,直接抛出异常
+	 *
+	 * @access public
+	 * @return String
+	 * @throws Typecho_Plugin_Exception
+	 */
 	 public static function activate()
-    {
+	{
 		$db = Typecho_Db::get();
-        $shortlinks = $db->getPrefix() . 'shortlinks';
-        $adapter = $db->getAdapterName();
-        if("Pdo_SQLite" === $adapter || "SQLite" === $adapter){
-           $db->query(" CREATE TABLE IF NOT EXISTS ". $shortlinks ." (
-               id INTEGER PRIMARY KEY, 
-               key TEXT,
-               target TEXT,
-               count NUMERIC)");
-        }
-        if("Pdo_Mysql" === $adapter || "Mysql" === $adapter){
-            $db->query("CREATE TABLE IF NOT EXISTS ". $shortlinks ." (
-                  `id` int(8) NOT NULL AUTO_INCREMENT,
-                  `key` varchar(64) NOT NULL,
-                  `target` varchar(10000) NOT NULL,
-                  `count` int(8) DEFAULT '0',
-                  PRIMARY KEY (`id`)
-                ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1");
-        }
+		$shortlinks = $db->getPrefix() . 'shortlinks';
+		$adapter = $db->getAdapterName();
+		if("Pdo_SQLite" === $adapter || "SQLite" === $adapter){
+		   $db->query(" CREATE TABLE IF NOT EXISTS ". $shortlinks ." (
+			   id INTEGER PRIMARY KEY, 
+			   key TEXT,
+			   target TEXT,
+			   count NUMERIC)");
+		}
+		if("Pdo_Mysql" === $adapter || "Mysql" === $adapter){
+			$db->query("CREATE TABLE IF NOT EXISTS ". $shortlinks ." (
+				  `id` int(8) NOT NULL AUTO_INCREMENT,
+				  `key` varchar(64) NOT NULL,
+				  `target` varchar(10000) NOT NULL,
+				  `count` int(8) DEFAULT '0',
+				  PRIMARY KEY (`id`)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1");
+		}
 
-        Helper::addAction('shortlinks', 'ShortLinks_Action');
-        Helper::addRoute('t', '/t/[key]/', 'ShortLinks_Action', 'baselink');
+		Helper::addAction('shortlinks', 'ShortLinks_Action');
+		Helper::addRoute('t', '/t/[key]/', 'ShortLinks_Action', 'baselink');
 		Helper::addRoute('go', '/go/[key]/', 'ShortLinks_Action', 'shortlink');
-        Helper::addPanel(2, 'ShortLinks/panel.php', '短链接', '短链接管理',   'administrator');
+		Helper::addPanel(2, 'ShortLinks/panel.php', '短链接', '短链接管理',   'administrator');
 		Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('ShortLinks_Plugin','replace');
 		Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('ShortLinks_Plugin','replace');
 		Typecho_Plugin::factory('Widget_Abstract_Comments')->contentEx = array('ShortLinks_Plugin','replace');
-        return('数据表 '.$shortlinks.' 创建成功, 插件已经成功激活!');
-    }
+		return('数据表 '.$shortlinks.' 创建成功, 插件已经成功激活!');
+	}
 	/**
-     * 禁用插件方法,如果禁用失败,直接抛出异常
-     *
-     * @static
-     * @access public
-     * @return String
-     * @throws Typecho_Plugin_Exception
-     */
-    public static function deactivate()
-    {
-        Helper::removeRoute('go');
+	 * 禁用插件方法,如果禁用失败,直接抛出异常
+	 *
+	 * @static
+	 * @access public
+	 * @return String
+	 * @throws Typecho_Plugin_Exception
+	 */
+	public static function deactivate()
+	{
+		Helper::removeRoute('go');
 		Helper::removeRoute('t');
-        Helper::removeAction('shortlinks');
-        Helper::removePanel(2, 'ShortLinks/panel.php');
+		Helper::removeAction('shortlinks');
+		Helper::removePanel(2, 'ShortLinks/panel.php');
 		return('短链接插件已被禁用，但是数据表并没有被删除');
-    }
+	}
 
-    /**
-     * 获取插件配置面板
-     *
-     * @access public
-     * @param Typecho_Widget_Helper_Form $form 配置面板
-     * @return void
-     */
-    public static function config(Typecho_Widget_Helper_Form $form)
-    {
+	/**
+	 * 获取插件配置面板
+	 *
+	 * @access public
+	 * @param Typecho_Widget_Helper_Form $form 配置面板
+	 * @return void
+	 */
+	public static function config(Typecho_Widget_Helper_Form $form)
+	{
 		$convert =  new Typecho_Widget_Helper_Form_Element_Radio('convert' , array('1'=>_t('开启'),'0'=>_t('关闭')),'1',_t('外链转内链'),_t('会帮你把外链转换成内链'));
 		$form->addInput($convert );
-    }
+	}
 
-    /**
-     * 个人用户的配置面板
-     *
-     * @access public
-     * @param Typecho_Widget_Helper_Form $form
-     * @return void
-     */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 	/**
-     * 外链转内链
-     *
-     * @access public
-     * @param $content
+	 * 个人用户的配置面板
+	 *
+	 * @access public
+	 * @param Typecho_Widget_Helper_Form $form
+	 * @return void
+	 */
+	public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+	/**
+	 * 外链转内链
+	 *
+	 * @access public
+	 * @param $content
 	 * @param $class
-     * @return $content
-     */
+	 * @return $content
+	 */
 	public static function replace($text, $widget, $lastResult) {
 		if(Typecho_Widget::widget('Widget_Options')->Plugin('ShortLinks')->convert == 1)  {
 			$text = empty($lastResult) ? $text : $lastResult;
