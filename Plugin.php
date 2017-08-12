4677d13a64a11828cf7fbb41efd5e1870706d046
benzbrake <coding.net@woai.ru>
2017-08-12 18:13:58 +0800

    Just Bump Version

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@
  *
  * @package ShortLinks
  * @author Ryan
- * @version 1.0.3
+ * @version 1.0.4
  * @link http://blog.iplayloli.com/typecho-plugin-shortlinks.html
  */
  class ShortLinks_Plugin implements Typecho_Plugin_Interface
