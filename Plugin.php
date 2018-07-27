b018d3927dedaefadaefcbc3c6230c8891127b34
Ryan <github-benzBrake@woai.ru>
2018-07-27 17:25:28 +0800

    修改版本号

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@
  *
  * @package ShortLinks
  * @author Ryan
- * @version 1.0.7
+ * @version 1.0.8
  * @link http://blog.iplayloli.com/typecho-plugin-shortlinks.html
  */
  class ShortLinks_Plugin implements Typecho_Plugin_Interface
