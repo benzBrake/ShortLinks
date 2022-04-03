3830a29d395ab1033ef2f5843dc6c89aa6e597d4
Ryan Lieu <github-benzBrake@woai.ru>
2022-04-03 11:56:25 +0800

    feat: 使用 Typecho 内置样式显示提示

--- panel.php
+++ panel.php
@@ -146,6 +146,58 @@ include 'footer.php';
 </style>
 <script type="text/javascript">
     $(document).ready(function () {
+        function notice(noticeText, noticeType, noticeClass = '.typecho-head-nav') {
+            var head = $(noticeClass),
+                p = $('<div class="message popup ' + noticeType + '">' +
+                    '<ul><li>' + noticeText + '</li></ul></div>'),
+                offset = 0;
+
+            if (head.length > 0) {
+                p.insertAfter(head);
+                offset = head.outerHeight();
+            } else {
+                p.prependTo(document.body);
+            }
+
+            function checkScroll() {
+                if ($(window).scrollTop() >= offset) {
+                    p.css({
+                        'position': 'fixed',
+                        'top': 0
+                    });
+                } else {
+                    p.css({
+                        'position': 'absolute',
+                        'top': offset
+                    });
+                }
+            }
+
+            $(window).scroll(function () {
+                checkScroll();
+            });
+
+            checkScroll();
+
+            p.slideDown(function () {
+                var t = $(this),
+                    color = '#C6D880';
+
+                if (t.hasClass('error')) {
+                    color = '#FBC2C4';
+                } else if (t.hasClass('notice')) {
+                    color = '#FFD324';
+                }
+
+                t.effect('highlight', {
+                    color: color
+                })
+                    .delay(5000).fadeOut(function () {
+                    $(this).remove();
+                });
+            });
+        }
+
         $('.operate-edit').click(function () {
             var tr = $(this).parents('tr'), t = $(this), id = tr.attr('id');
             var value = $('#e-' + id).html();
@@ -162,8 +214,9 @@ include 'footer.php';
                         if (data === 'success') {
                             $('#e-' + id).html($('#t-' + id).val());
                             $("[href='#" + id + "']").show();
+                            notice('<?php _e("修改链接成功"); ?>', 'success');
                         } else {
-                            alert('请输入有效链接');
+                            notice('<?php _e("请输入有效链接"); ?>', 'error');
                         }
                     }
                 });
